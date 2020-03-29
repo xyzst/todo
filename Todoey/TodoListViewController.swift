1689d020c2d675620b12fe72eda06316ed41d5fd
Darren Rambaud <xyzst@users.noreply.github.com>
2020-03-29 16:37:44 -0500

    Create feature to add a single item
    - Add table view delegate methods to enhance the UI slightly (add cell fade out effect, add check mark accessory for each cell)
    - Add IBAction to prompt user to add a single item to the list, refresh table view after user inputs valid data
    
    Related to: none

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -7,11 +7,9 @@
 //
 
 import UIKit
-//import K
 
 class TodoListViewController: UITableViewController {
-    
-    let items = ["get lemons", "buy eggs"]
+    var items = ["get lemons", "buy eggs"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +30,36 @@ class TodoListViewController: UITableViewController {
         return c
     }
     
+    // MARK - Table View Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = cell.accessoryType == .none ? .checkmark : .none
+        }
+        tableView.deselectRow(at: indexPath, animated: true) // Not performing the fade out effect?
+    }
+    
+    // MARK - Add New Items
+    
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // once the user clicks button, send request to api
+            if let item = textField.text {
+                self.items.append(item)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (alertText) in
+            alertText.placeholder = "Create new item"
+            textField = alertText
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
